3e34c5dc8b036a9331fef1fc88c5dd684f766b4e
Kumori <tauen8@gmail.com>
2023-05-23 23:41:03 -0500

    created variables to select each button and assign ids, created event listener structure with Jquery for each question

---
+++ script.js
@@ -0,0 +1,98 @@
+//Make variable for buttons for listeners
+var question1AWrong = document.querySelector("#q1-A-w");
+var question1BRight= document.querySelector("#q1-B-R");
+var question1CWrong = document.querySelector("#q1-C-W");
+var question1DWrong = document.querySelector("#q1-D-W");
+
+var question2AWrong = document.querySelector("#q2-A-w");
+var question2BWrong= document.querySelector("#q2-B-W");
+var question2CRight = document.querySelector("#q2-C-R");
+var question2DWrong = document.querySelector("#q2-D-W");
+
+var question3ARight = document.querySelector("#q3-A-R");
+var question3BWrong= document.querySelector("#q3-B-W");
+var question3CWrong = document.querySelector("#q3-C-W");
+var question3DWrong = document.querySelector("#q3-D-W");
+
+var question4ARight = document.querySelector("#q4-A-R");
+var question4BWrong= document.querySelector("#q4-B-W");
+var question4CWrong = document.querySelector("#q4-C-W");
+var question4DWrong = document.querySelector("#q4-D-W");
+
+var question5AWrong = document.querySelector("#q5-A-w");
+var question5BWrong= document.querySelector("#q5-B-W");
+var question5CRight = document.querySelector("#q5-C-R");
+var question5DWrong = document.querySelector("#q5-D-W");
+
+//Make event listeners for all buttons
+//Question 1
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+
+//Question 2
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+
+  //Question 3
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+
+  //Question 4
+
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+
+  //Question 5
+
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
+alertButtonEl.on('click', function () {
+    alert('Hello World');
+  });
